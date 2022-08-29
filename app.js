50897c61b8aa9714d63e36a9c78d807cec311da2
VodeniZeko <edvin.saletovic@pm.me>
2022-08-29 00:38:12 -0500

    418 joke

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const express = require('express');
+const express = require("express");
 const server = express();
 const port = 3000;
 
-server.get('/',(req,res) => {
-res.send('hello from app.js');
+server.get("/", (req, res) => {
+  res.send("hello from app.js");
 });
 
-server.listen(port, () => {console.log(`listening at port ${port}`)});
+server.get("/teapot", (req, res) => {
+  res.set("X-full-stack", "learnin");
+  res.status(418);
+  res.send("this is the tea pot joke");
+});
 
+server.listen(port, () => {
+  console.log(`listening at port ${port}`);
+});
