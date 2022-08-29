7aaa8d4c00800bb1f7224a229b64be5f58b2314f
digital_ocean_app_user <saletovic.developer@gmail.com>
2022-08-29 02:38:19 +0000

    add files

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const server = express();
+const port = 3000;
+
+server.get('/',(req,res) => {
+res.send('hello from app.js');
+});
+
+server.listen(port, () => {console.log(`listening at port ${port}`)});
